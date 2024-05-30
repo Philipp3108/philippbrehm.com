e21623206c27a588036f6b347bfbe9de738f55c0
Philipp3108 <philipp.brehm3108@gmx.de>
2024-05-30 17:47:11 +0200

    Portfolio

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Philipp's Portfolio",
+  description: "My Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider></body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/Grid";
+import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/components/RecentProjects";
+import { Approach } from "@/components/Approach";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon: <FaHome />}
+        ]} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Approach />
+      </div>
+    </main>
+  );
+}
