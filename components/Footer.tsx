5b74942a8c166c3584b3fe3e0c5667d2cf2bedbc
Philipp <153946663+Philipp3108@users.noreply.github.com>
2024-06-29 23:46:35 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
             <h1 className='heading lg:max-w-[45vw]'>
                 Ready to take <span className='text-purple'>your</span> digital presence to the next level?
             </h1>
-            <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let's discuss how I can help you achieve your goals.</p>
+            <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
             <a href="mailto:contact@philippbrehm.com">
                 <MagicButton
                     title='Let&apos;s get in toutch'
