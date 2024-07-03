0928d04d212c85c98f7bba3ce269492df72a96d1
Philipp <153946663+Philipp3108@users.noreply.github.com>
2024-07-03 07:33:02 +0200

    Aktualisieren von Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
             <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
             <a href="mailto:contact@philippbrehm.com">
                 <MagicButton
-                    title='Let&apos;s get in toutch'
+                    title='Let&apos;s get in touch'
                     icon={<FaLocationArrow />}
                     position='right'
                 />
