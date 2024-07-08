b94315e501d83559cca8cd549ff721ef4083c5a0
Philipp3108 <philipp.brehm3108@gmx.de>
2024-07-08 17:16:17 +0200

    Commiz

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-37F1PP6C4Z"></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-37F1PP6C4Z');`}
+      </Script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider
             attribute="class"
