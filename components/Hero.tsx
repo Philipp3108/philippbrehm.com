a7169e7250635903a0d7b38917e342dfa502a5e3
Philipp <153946663+Philipp3108@users.noreply.github.com>
2025-04-22 05:14:10 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,11 +20,11 @@ const Hero = () => {
 
         <div className='flex justify-center relative my-20 z-10'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic Web Magic with Next.js</h2>
+                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic Back-End Systems with Node.js</h2>
 
                 <TextGenerateEffect 
                     className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                    words='Transforming Concepts into Seamless User Experiences'
+                    words='Transforming Concepts into Efficient Back-End Systems'
                 />
 
                 <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
