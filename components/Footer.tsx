8eb436819af2dbd499281dc34f2a0cb21da81b42
Philipp <153946663+Philipp3108@users.noreply.github.com>
2025-04-22 05:13:43 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <footer className='w-full pb-10 mb-[100px] md:mb-5' id='contact'>
         <div className='flex flex-col items-center'>
             <h1 className='heading lg:max-w-[45vw]'>
-                Ready to take <span className='text-purple'>your</span> digital presence to the next level?
+                Got some <span className='text-purple'>ideas</span> in mind to work together?
             </h1>
             <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
             <a href="mailto:contact@philippbrehm.com">
