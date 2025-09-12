43145e0e86f6d8da3320c470824262871e52975c
Philipp <153946663+Philipp3108@users.noreply.github.com>
2025-09-12 10:00:50 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[
-          {name: 'Home', link: '/#home', icon: <FaHome />},
-          {name: 'About', link: '/#about', icon: <FaHome />},
-          {name: 'Projects', link: '/#projects', icon: <FaHome />},
-          {name: 'Approach', link: '/#approach', icon: <FaHome />}
+          {name: 'Home', link: '/#home'},
+          {name: 'About', link: '/#about'},
+          {name: 'Projects', link: '/#projects'},
+          {name: 'Approach', link: '/#approach'}
         ]} />
         <Hero />
         <Grid />
