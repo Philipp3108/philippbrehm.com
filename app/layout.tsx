784e27b1e4a56ae97ec20e1bb195b1ac0126d542
Philipp <153946663+Philipp3108@users.noreply.github.com>
2026-04-15 17:27:42 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,34 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Philipp's Portfolio",
-  description: "My Portfolio",
+  title: "Portfolio",
+  description: "Philipp Brehm",
+  metadataBase: new URL("https://philippbrehm.com/"),
+
+  openGraph: {
+    title: "Philipp Brehm",
+    description: "Portfolio",
+    url: "https://philippbrehm.com/",
+    siteName: "Philipp Brehm",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 1200,
+        height: 630,
+        alt: "Philipp Brehm",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Philipp Brehm",
+    description: "Portfolio",
+    images: ["/favicon.ico"],
+    creator: "@philippbrehm_",
+  },
 };
 
 export default function RootLayout({
